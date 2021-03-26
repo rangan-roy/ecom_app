5b543fdcffcb8398598d154949aae6f578de56da
Rangan Roy <ranganroy567@gmail.com>
2021-03-26 10:26:32 +0600

    added category names and images to home_page.dart

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:ecom_app/builders/app_bar_builder.dart';
 import 'package:ecom_app/widgets/drawer_widget.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -13,6 +14,144 @@ class _HomePageState extends State<HomePage> {
     return Scaffold(
       appBar: buildAppBar('E-commerce'),
       drawer: DrawerWidget(),
+      body: ListView(
+        children: [
+          // category names and images
+          GridView.count(
+            padding: EdgeInsets.symmetric(
+              horizontal: 10,
+              vertical: 15,
+            ),
+            // allows to use GridView() inside ListView()
+            shrinkWrap: true,
+            // makes GridView() scrollable (like a normal ListView() child) when
+            // we hold the GridView() and try to scroll
+            physics: ClampingScrollPhysics(),
+            crossAxisCount: 3,
+            mainAxisSpacing: 5,
+            crossAxisSpacing: 5,
+            children: [
+              Card(
+                elevation: 10,
+                child: Stack(
+                  children: [
+                    ClipRRect(
+                      borderRadius: BorderRadius.circular(4),
+                      child: Image.asset(
+                        'images/user_profile_cover.jpg',
+                        fit: BoxFit.cover,
+                        height: double.maxFinite,
+                        width: double.maxFinite,
+                      ),
+                    ),
+                    Positioned(
+                      child: Container(
+                        padding: EdgeInsets.all(5),
+                        alignment: Alignment.center,
+                        height: double.maxFinite,
+                        width: double.maxFinite,
+                        color: Colors.white30,
+                        child: Text(
+                          'T-shirt',
+                          style: Theme.of(context).textTheme.headline6,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Card(
+                elevation: 10,
+                child: Stack(
+                  children: [
+                    ClipRRect(
+                      borderRadius: BorderRadius.circular(4),
+                      child: Image.asset(
+                        'images/user_profile_cover.jpg',
+                        fit: BoxFit.cover,
+                        height: double.maxFinite,
+                        width: double.maxFinite,
+                      ),
+                    ),
+                    Positioned(
+                      child: Container(
+                        padding: EdgeInsets.all(5),
+                        alignment: Alignment.center,
+                        height: double.maxFinite,
+                        width: double.maxFinite,
+                        color: Colors.white30,
+                        child: Text(
+                          'T-shirt',
+                          style: Theme.of(context).textTheme.headline6,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Card(
+                elevation: 10,
+                child: Stack(
+                  children: [
+                    ClipRRect(
+                      borderRadius: BorderRadius.circular(4),
+                      child: Image.asset(
+                        'images/user_profile_cover.jpg',
+                        fit: BoxFit.cover,
+                        height: double.maxFinite,
+                        width: double.maxFinite,
+                      ),
+                    ),
+                    Positioned(
+                      child: Container(
+                        padding: EdgeInsets.all(5),
+                        alignment: Alignment.center,
+                        height: double.maxFinite,
+                        width: double.maxFinite,
+                        color: Colors.white30,
+                        child: Text(
+                          'T-shirt',
+                          style: Theme.of(context).textTheme.headline6,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Card(
+                elevation: 10,
+                child: Stack(
+                  children: [
+                    ClipRRect(
+                      borderRadius: BorderRadius.circular(4),
+                      child: Image.asset(
+                        'images/user_profile_cover.jpg',
+                        fit: BoxFit.cover,
+                        height: double.maxFinite,
+                        width: double.maxFinite,
+                      ),
+                    ),
+                    Positioned(
+                      child: Container(
+                        padding: EdgeInsets.all(5),
+                        alignment: Alignment.center,
+                        height: double.maxFinite,
+                        width: double.maxFinite,
+                        color: Colors.white30,
+                        child: Text(
+                          'T-shirt',
+                          style: Theme.of(context).textTheme.headline6,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+
+        ],
+      ),
     );
   }
 }
