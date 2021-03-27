ebffaca2201b7b907efdc3d7286a206f3449400f
Rangan Roy <ranganroy567@gmail.com>
2021-03-27 18:42:03 +0600

    added image slider on product details page

--- lib/pages/product_details.dart
+++ lib/pages/product_details.dart
@@ -1,5 +1,6 @@
 import 'package:ecom_app/builders/app_bar_builder.dart';
 import 'package:flutter/material.dart';
+import 'package:carousel_slider/carousel_slider.dart';
 
 class ProductDetailsPage extends StatefulWidget {
   @override
@@ -11,6 +12,23 @@ class _ProductDetailsPageState extends State<ProductDetailsPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: buildAppBar('Product Name', context, true),
+      body: CarouselSlider(
+        options: CarouselOptions(
+          height: 200,
+          enlargeCenterPage: true,
+        ),
+        items: [
+          ClipRRect(
+            borderRadius: BorderRadius.circular(10),
+            child: Image.asset(
+              'images/t-shirt.jpg',
+              fit: BoxFit.cover,
+              width: double.maxFinite,
+              height: double.maxFinite,
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
